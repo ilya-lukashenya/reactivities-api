1d151e7e67138b7c73e687b31e84f7fd84264b2d
ilya-lukashenya <il.lukashenya@gmail.com>
2022-07-14 21:35:19 +0300

    Lesson 12 done.

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -28,7 +28,6 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Console.WriteLine(_config.GetConnectionString("DefaultConnection"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {

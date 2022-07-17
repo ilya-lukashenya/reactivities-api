3b6f812644599b192ea4db3f3a71d9dee30a691b
ilya-lukashenya <il.lukashenya@gmail.com>
2022-07-17 18:59:18 +0300

    End of Section 3

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -37,6 +37,17 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +64,9 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization(); 
 
             app.UseEndpoints(endpoints =>
             {
